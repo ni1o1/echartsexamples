7e992f4b02c1fc519160be2484632581fe79f5ba
余庆 <yuqing@yuqingdeMacBook-Air-4.local>
2021-10-08 15:43:52 +0800

    update

---
+++ run.py
@@ -0,0 +1,10 @@
+from flask import Flask,render_template
+app = Flask(__name__,
+static_folder = '',
+template_folder = '')
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+if  __name__  ==  '__main__':
+    from werkzeug.serving import run_simple
+    run_simple('127.0.0.1', 8080, app)

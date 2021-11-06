667965b78a1d046d59df5fe989775f034f9d1c41
余庆 <yuqing@yuqingdeMacBook-Air-4.local>
2021-11-06 16:01:54 +0800

    test

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ static_folder = '',
 template_folder = '')
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('3dbuildings.html')
 if  __name__  ==  '__main__':
     from werkzeug.serving import run_simple
     run_simple('127.0.0.1', 8080, app)
